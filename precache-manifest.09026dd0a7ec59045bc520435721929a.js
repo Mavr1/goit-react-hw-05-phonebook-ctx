2b3b3893ecf33ff957916323375c9733288ce724
Mavr1 <mavrodiy.m.e@gmail.com>
2020-07-08 19:41:39 +0300

    Updates

---
+++ precache-manifest.09026dd0a7ec59045bc520435721929a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8932f842efa365c32e15eb77184c1507",
+    "url": "/goit-react-hw-05-phonebook-ctx/index.html"
+  },
+  {
+    "revision": "b2ab0dccc83f37af7adb",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/css/main.72dcc3cf.chunk.css"
+  },
+  {
+    "revision": "0c7f800742ad80197a56",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.b415e143.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/2.b415e143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ab0dccc83f37af7adb",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/main.8655250c.chunk.js"
+  },
+  {
+    "revision": "d281f072a4e1a9520414",
+    "url": "/goit-react-hw-05-phonebook-ctx/static/js/runtime-main.2a5452f8.js"
+  }
+]);
